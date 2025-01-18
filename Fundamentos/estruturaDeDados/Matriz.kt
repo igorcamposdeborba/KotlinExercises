fd59360e48b3702d566d833f43f5138c3046a2b4
igorcamposdeborba <igor_campos5@hotmail.com>
2025-01-18 16:07:30 -0300

    Kotlin

---
+++ Fundamentos/estruturaDeDados/Matriz.kt
@@ -0,0 +1,14 @@
+package fundamentos.estruturaDeDados
+
+fun main() {
+
+    val matriz: Array<Array<Int?>> = Array(3){ arrayOfNulls<Int>(3) }
+
+    for (i in matriz.indices){
+        for (j in matriz[i].indices){
+            matriz[i][j] = (i + 1) * (j + 1)
+        }
+    }
+
+    matriz.forEach { i -> i.forEach { j -> println(j) } }
+}
