38135bffc01bf3361b7e2a554cfed06ccf904aa1
igorcamposdeborba <igor_campos5@hotmail.com>
2025-01-18 12:58:17 -0300

    Kotlin

--- Fundamentos/funcoes/Map.kt
+++ Fundamentos/funcoes/Map.kt
@@ -1,13 +1,26 @@
 package fundamentos.funcoes
-
 import java.util.Locale
+
+class Produto(val nome: String, val preco: Double)
+
+val materialEscolar = listOf<Produto>(
+                            Produto("Caderno", 22.40),
+                            Produto("Lapis", 0.70),
+                            Produto("Apontador", 1.20)
+)
 
 fun main() {
     val alunos: List<String> = arrayListOf("Igor", "Roberto", "Luiza", "Laura")
 
     alunos.map { it.uppercase(Locale("pt", "BR")) }
-                          .apply { print(this) }
+                          .apply { println(this) }
 
+    val totalizar = { acumulador: Double, atual: Double -> acumulador + atual} // interface funcional
+    val precoTotal = materialEscolar.map { it.preco }
+                                    .reduce(totalizar)
+                                    .apply { println("Preco medio: R$" + this / materialEscolar.size) }
+    // apply é uma nova "stream" porque reduce retorna um valor generics. Por isso consigo imprimir em apply e em println da variável a média
 
+    println("Media de preco: R$${precoTotal/ materialEscolar.size}")
 
 }
