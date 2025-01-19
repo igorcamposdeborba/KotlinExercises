1bf4d9e62162bdae6421d031a2317954fbf2bc08
igorcamposdeborba <igor_campos5@hotmail.com>
2025-01-19 19:19:41 -0300

    Kotlin

---
+++ Fundamentos/orientacaoObjetos/Recursao.kt
@@ -0,0 +1,11 @@
+package fundamentos.orientacaoObjetos
+
+fun fatorial(num: Int): Int = when (num){
+    in 0..1 -> 1
+    in 2.. Int.MAX_VALUE -> num * fatorial(num - 1)
+    else -> throw IllegalArgumentException("Numero negativo")
+}
+
+fun main() {
+    println("Resultado: ${fatorial(-5)}")
+}
